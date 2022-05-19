a341513f2b25622f93c06ed4855b77b38053e501
Claire Utrecht <utrechtc@gmail.com>
2022-05-18 20:40:50 -0400

    committing header app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './components/Homepage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Homepage/>
     </div>
   );
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Header() {
+
+    return (
+        <div className = "Header">
+            <h1>THE EVENT FINDER!!!!!</h1>
+        </div>
+    )
+}
