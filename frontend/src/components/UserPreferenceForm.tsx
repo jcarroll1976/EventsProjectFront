3b4ab20a9e21463e2a0cb28b1460fa0a31194282
Shiretha Bellamy <sade.bell937@gmail.com>
2022-05-19 21:07:39 -0400

    user preference form updates

--- frontend/src/components/UserPreferenceForm.tsx
+++ frontend/src/components/UserPreferenceForm.tsx
@@ -2,6 +2,9 @@ import {FormEvent, useState} from 'react';
 import './UserPreferenceForm.css';
 import {Event} from "../models/eventModels";
 
+//Type event needs to be changed to user preference type
+
+
 
 
 interface Props {
@@ -14,6 +17,8 @@ function UserPreferenceForm({onSubmit}: Props) {
     const [type, settype] =useState('');
     const [capacity, setcapacity]= useState('');
     const [postalCode, setpostalCode]= useState('');
+    const [genres, setgenres] = useState('');
+
     
     
 
@@ -24,11 +29,15 @@ function UserPreferenceForm({onSubmit}: Props) {
         type:type,
         venue: {capacity:Number(capacity),postal_code: postalCode },
         
+        
+
+        
     }
     onSubmit(event);
     settype('');
     setcapacity('');
     setpostalCode('');
+    setgenres('');
 
 }
 
@@ -40,22 +49,22 @@ function UserPreferenceForm({onSubmit}: Props) {
 
                 <p>
                     <label htmlFor='postalcode'>What is your desired location? </label>
-                    <input type ="number"  placeholder = "zipcode" value = {postalCode} onChange =  {e => setpostalCode(e.target.value)}></input>
+                    <input type ="number"  placeholder = "zipcode" value = {postalCode} onChange =  {e => setpostalCode(e.target.value)} required></input>
 
                     <label htmlFor='type'>Which Type of Events Are You Looking For? Please select all that apply</label>
-                    <input type ="checkbox" value = {type} onChange =  {e => settype(e.target.value)}></input>
+                    <input type ="checkbox" value = {type} onChange =  {e => settype(e.target.value)} required></input>
                     <select>
-                     <option value="Baseball">Baseball</option>
-                     <option value="Football">Football</option>
-                     <option value="Fighting/WWE">Fighting/WWE</option>
-                     <option value="Golf">Golf</option>
-                     <option value ="Hockey">Hockey</option>
-                     <option value= "Soccer">Soccer</option>
+                     <option value="Sports">Sports</option>
+                     <option value="Concerts">Concerts</option>
+                     <option value="Comedy">Comedy</option>
+                     <option value="Broadway">Broadway</option>
+                     <option value ="HorseRacing">HorseRacing</option>
+                     <option value= "Monster Trucks">Monster Trucks</option>
                     </select>
                     
 
-                    <label htmlFor='type'>What Kind Of Music Do You Like? Please select all that apply</label>
-                    <input type ="checkbox" value = {type} onChange =  {e => settype(e.target.value)}>
+                    <label htmlFor='genre'>What Genre Of Music Do You Like? Please select all that apply</label>
+                    <input type ="checkbox" value = {genres} onChange =  {e => setgenres(e.target.value)}>
                         <option value="Hard Rock">Hard Rock</option>
                         <option value="Reggae">Reggae</option>
                         <option value="Hip Hop">Hip Hop</option>
@@ -65,10 +74,31 @@ function UserPreferenceForm({onSubmit}: Props) {
                         <option value="Folk">Folk</option>
                         <option value="Punk">Punk</option>
                         <option value="Soul">Soul</option>
-                        
+                        <option value="Latin">Latin</option>
+                        <option value="Classical">Classical</option>
+                        <option value="Jazz">Jazz</option>
+                        <option value="Blues">Blues</option>
+                        <option value="Rap">Rap</option>
+                        <option value="Indie">Indie</option>
+                        <option value="Country">Country</option>
+                        <option value="Classic Rock">Classic Rock</option>
+                        <option value="Alternative">Alternative</option>
+                    </input>
 
+                    <label htmlFor='capacity'>What Type Of Sports Do You Enjoy?</label>
+                    <input type ="checkbox" value = {capacity} onChange =  {e => setcapacity(e.target.value)}>
+                        <option value="Baseball">Baseball</option>
+                        <option value="Football">Football</option>
+                        <option value="Basketball">Basketball</option>
+                        <option value="Fighting/WWE">Fighting/WWE</option>
+                        <option value="Golf">Golf</option>
+                        <option value="Hockey">Hockey</option>
+                        <option value="Soccer">Soccer</option>
 
                     </input>
+
+
+
 
                     <label htmlFor='capacity'>Is It A Certain Venue Size You Prefer?</label>
                     <input type ="checkbox" value = {capacity} onChange =  {e => setcapacity(e.target.value)}>
