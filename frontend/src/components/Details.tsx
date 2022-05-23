fdf4c87006f1a532815197425c500cd132a86336
Jordan Salgado <JDSalgado41803@gmail.com>
2022-05-22 22:36:03 -0400

    commit

--- frontend/src/components/Details.tsx
+++ frontend/src/components/Details.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
 
-
+// whenever a user clicks details button link to this page
 
 export default function Details() {
 
 
   return (
+      //use id from api
     <div className='Details'>
      <h1>Event Details</h1> 
      <p>image goes here!</p>
      <p>Details go here!</p>
      <button>Save</button>
      <button>Share</button>
-     <button>Review</button>  
+     <button>Review</button> 
     </div>
   )
 }
+//be able to link back to the last page
