2ad223cac4820ec9e5b045dbbaa86349077a51b8
amandell2 <amanda.mandell2@gmail.com>
2022-05-29 11:57:21 -0400

    shorted singleEvent when multiple permormers are listed

--- frontend/src/components/SingleEvent.tsx
+++ frontend/src/components/SingleEvent.tsx
@@ -16,7 +16,9 @@ export default function SingleEvent({event}:Props){
             <h2>{event.title}</h2>
             {(event.taxonomies && event.taxonomies.length >= 0) && <p>Event Type: {event.taxonomies[0].name}</p>}
             {(event.performers && event.performers.length >= 0) && <img src={event.performers[0].image}/>}
-            {event.performers?.map((info, i)=> <p>Performer: {info.name}</p>)}
+            {((event.performers && event.performers.length > 1) && <p>Performer: {event.performers[0].name} and More! Click below to see all performers!</p>)||
+            ((event.performers && event.performers.length === 1) && <p>Performer: {event.performers[0].name}</p>)}
+            {/*event.performers?.map((info, i)=> <p>Performer: {info.name}</p>)*/}
             <Link to={`/Details/${event.id}`}>
                 <button>Click here for more details!</button>
             </Link>
