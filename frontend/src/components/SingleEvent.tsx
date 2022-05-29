1dcccdfe0152ff824a37274d6788ff1cb9d96ebd
amandell2 <amanda.mandell2@gmail.com>
2022-05-29 12:20:12 -0400

    added 'performers' if more than 1 performer

--- frontend/src/components/SingleEvent.tsx
+++ frontend/src/components/SingleEvent.tsx
@@ -16,7 +16,7 @@ export default function SingleEvent({event}:Props){
             <h2>{event.title}</h2>
             {(event.taxonomies && event.taxonomies.length >= 0) && <p>Event Type: {event.taxonomies[0].name}</p>}
             {(event.performers && event.performers.length >= 0) && <img src={event.performers[0].image}/>}
-            {((event.performers && event.performers.length > 1) && <p>Performer: {event.performers[0].name} and More! Click below to see all performers!</p>)||
+            {((event.performers && event.performers.length > 1) && <p>Performers: {event.performers[0].name} and More! Click below to see all performers!</p>)||
             ((event.performers && event.performers.length === 1) && <p>Performer: {event.performers[0].name}</p>)}
             <Link to={`/Details/${event.id}`}>
                 <button>Click here for more details!</button>
