6059a736064ed84b5ef54127084abb06a44d6e0c
amandell2 <amanda.mandell2@gmail.com>
2022-05-29 11:47:45 -0400

    Login page css begun

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,12 +3,26 @@ import { signInWithGoogle, signOut } from "../firebaseconfig";
 import Homepage from './Homepage';
 import {useParams} from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
+import "./Login.css";
+import background from "../pexels-maegan-neufeld-342411.jpg"
 
 export default function Login() {
     const {user} = useContext(AuthContext)
-    return (
-        <div className = "LoginComponent">
 
+    const myStyle={
+        backgroundImage: `url(${background})`,
+        height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        opacity: 0.5
+    };
+
+    return (
+        <div className = "LoginComponent" style={myStyle}>
+            <h1>Welcome to your personal Event Generator!</h1>
+            <h2>Please sign in below.</h2>
             {user ?
             <div><Homepage/></div>:
             <div><button onClick={signInWithGoogle}>Sign in with Google</button></div>}
