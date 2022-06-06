532c26a8283e496bba1e75267630b68975c5a27d
amandell2 <amanda.mandell2@gmail.com>
2022-06-06 00:26:11 -0400

    changed details css names to prevent styles changing in other components

--- frontend/src/components/Details.tsx
+++ frontend/src/components/Details.tsx
@@ -43,44 +43,33 @@ function addReview(userReview:Review):void{
 
   //const foundEvent = data.find((event) => event.id === id)
   return (
-    <div className='Details'>
-    <h1>{eventById?.title}</h1>
-    <img src={eventById?.performers![0].image}></img>
+    <div className='Details_Container'>
+      <h1>{eventById?.title}</h1>
+    <div className="Details_SideBySide_Container">
+      <img className='Details_img' src={eventById?.performers![0].image}></img>
+      <main className ='EventDetails'>
+          <p>This event is a {eventById?.type} event.</p>
+          <p>The venue is at {eventById?.venue?.name} and has a max capacity of {eventById?.venue?.capacity}.</p> 
+          <p>The event will be at {eventById?.venue?.address}, {eventById?.venue?.city}, {eventById?.venue?.state}, {eventById?.venue?.country} {eventById?.venue?.postal_code}.</p>
+          <ul>Performers included at event:</ul>
+          {eventById?.performers?.map((info, i)=> 
+          <li>{info.name}</li>)}
+        <a href={eventById?.url}>
+        <button className="Details_Btn" >Get your tickets with Seat Geek here!</button> 
+        </a>
+      </main>
 
-    <div className ='EventDetails'>
-
-      <p>This event is a {eventById?.type} event.</p>
-      <p>The venue is at {eventById?.venue?.name} and has a max capacity of {eventById?.venue?.capacity}.</p> 
-      <p>The event will be at {eventById?.venue?.address}, {eventById?.venue?.city}, {eventById?.venue?.state}, {eventById?.venue?.country} {eventById?.venue?.postal_code}.</p>
-      <ul>Performers included at event:</ul>
-      {eventById?.performers?.map((info, i)=> 
-      <li>{info.name}</li>)}
-
-      <div className='Buttons'/>
-     <button>Save</button>
-     <button>Share</button>
-     <button>Review</button>
-     <div/>
+     </div>
      
-     <div/>
-     <Link to={`/Login`}>
-       <button>Back to the main menu</button>
-     </Link>
-      </div>
-
-     <a href={eventById?.url}>
-     <button>Get your tickets with Seat Geek here!</button> 
-     </a>
-     
-     
-     <p>Event Reviews</p>
-     <button onClick= {() => setShowReviewForm(true)}>Leave A Review!</button>
-      {showReviewForm &&
-            <UserReviewForm onSubmit={addReview}/>}
-      {reviews.map((eventReview, i)=> 
-            <SingleUserReview review={eventReview}/>
-        )}
-
+     <section className="Details_ReviewSection">
+      <h2 className="Details_ReviewSectionTitle">Event Reviews</h2>
+      <button className="Details_Btn" onClick= {() => setShowReviewForm(true)}>Leave A Review!</button>
+        {showReviewForm &&
+              <UserReviewForm onSubmit={addReview}/>}
+        {reviews.map((eventReview, i)=> 
+              <SingleUserReview review={eventReview}/>
+          )}
+    </section>
     </div>
   )
 }
